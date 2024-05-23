3eb6c41c1738b1d439f8c0413fff6c4b42eff8a6
qbyz <143455729+qbyz@users.noreply.github.com>
2024-05-23 08:29:15 -0400

    Update main.py
    used selenium to try but I now need to solve the webdriver problem (arc doesn't have a webdriver integration)

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 url = "https://fiestafarms.ca/employment-opportunities"
 def search(url:str):
@@ -15,22 +21,19 @@ def search(url:str):
             print(instances[j-1])
         j += 1
 search(url)
+
+service = Service(ChromeDriverManager().install())
+driver = webdriver.chrome(service=service)
+
 url = "https://bestbuycanada.wd3.myworkdayjobs.com/BestBuyCA_Career?geotagText=CA/M6G%204B7/Toronto&distance=677b3630ee5f01a8d7424fdbdb0724d4&timeType=540b7b97140b01bb879ee98c7d1a8400"
-def search2(url):
-    page = urlopen(url)
-    html = page.read().decode('utf-8')
-    soup = BeautifulSoup(html, "html.parser")
-    instances = soup.find_all("str")
-    print(instances)
-    j = 0
-    for i in instances:
-        if "Part-time" in i:
-            print("success")
-            print(instances[j - 1])
-        j += 1
-search2(url)
-headers = { 'User-Agent': 'Mozilla/5.0', 'Accept': 'application/json,application/xml'}
-# I don't know what this code does-
-r = requests.get('https://bestbuycanada.wd3.myworkdayjobs.com/BestBuyCA_Career?geotagText=CA/M6G%204B7/Toronto&distance=677b3630ee5f01a8d7424fdbdb0724d4&timeType=540b7b97140b01bb879ee98c7d1a8400', headers=headers)
-links = ['https://wd1.myworkdaysite.com' + i['title']['commandLink'] for i in r.json()["body"]['children'][0]['children'][0]['listItems']]
-print(links)
+driver.get(url)
+
+wait = WebDriverWait(driver, 10)
+wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".gwt-Anchor")))
+
+job_listings = driver.find_elements(By.CSS_SELECTOR, ".gwt-anchor")
+
+for job in job_listings:
+    print(job.text)
+
+driver.quit()
