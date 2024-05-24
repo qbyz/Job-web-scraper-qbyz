c4ee96bcb7cdc9158f19dcd75c717714d2d0b189
Liam <143455729+qbyz@users.noreply.github.com>
2024-05-24 07:52:12 -0400

    Update main.py
    Used selenium on indigo and fixed css selectors for job titles/descriptions

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 url = "https://fiestafarms.ca/employment-opportunities"
-def search(url:str):
+def fiesta(url:str):
     page = urlopen(url)
     html = page.read().decode('utf-8')
     soup = BeautifulSoup(html, "html.parser")
@@ -19,23 +19,32 @@ def search(url:str):
             print("success")
             print(instances[j-1])
         j += 1
-search(url)
+fiesta(url)
 
-url = "https://bestbuycanada.wd3.myworkdayjobs.com/BestBuyCA_Career?geotagText=CA/M6G%204B7/Toronto&distance=677b3630ee5f01a8d7424fdbdb0724d4&timeType=540b7b97140b01bb879ee98c7d1a8400"
+url = "https://bestbuycanada.wd3.myworkdayjobs.com/BestBuyCA_Career?geotagText=CA/M6G%204B7/Toronto&distance=677b3630ee5f01cec31b4fdbdb0723d4&timeType=540b7b97140b01bb879ee98c7d1a8400"
 selector = ".css-19uc56f"
-def bestBuy(url:str, selector:str):
+def bestBuy(url:str, selector:str, desc_selector:str):
     service = Service(GeckoDriverManager().install())
     driver = webdriver.Firefox(service=service)
 
     driver.get(url)
 
-    wait = WebDriverWait(driver, 10)
-    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, selector)))
+    wait = WebDriverWait(driver, 20)
+    wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, selector)))
 
     job_listings = driver.find_elements(By.CSS_SELECTOR, selector)
-
-    for job in job_listings:
-        print(job.text)
+    if url != "https://bestbuycanada.wd3.myworkdayjobs.com/BestBuyCA_Career?geotagText=CA/M6G%204B7/Toronto&distance=677b3630ee5f01cec31b4fdbdb0723d4&timeType=540b7b97140b01bb879ee98c7d1a8400":
+        job_descs = driver.find_elements(By.CSS_SELECTOR, desc_selector)
+        for job, desc in zip(job_listings, job_descs):
+            print("Job Title:", job.text)
+            print("Job Description:", desc.text)
+    else:
+        for job in job_listings:
+            print(job.text)
 
     driver.quit()
-bestBuy(url, selector)
+bestBuy(url, selector, " ")
+url = "https://careers.smartrecruiters.com/IndigoBooksMusic/retail?search=toronto"
+selector = ".details-title.job-title.link--block-target"
+descselector = ".details-desc.job-desc"
+bestBuy(url,selector,descselector)
